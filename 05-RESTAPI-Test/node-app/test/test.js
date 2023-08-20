4282b4b1effcaa263b8d640f2b602d3bbde82f76
alex <907785012@qq.com>
2023-08-20 15:31:41 +0800

    Add mocha assert

--- 05-RESTAPI-Test/node-app/test/test.js
+++ 05-RESTAPI-Test/node-app/test/test.js
@@ -1,8 +1,11 @@
 import { expect } from "expect";
 import request from "supertest";
 import app from "../app.js";
+import assert from "assert";
 
 const response = await request(app).get("/user/23");
 
+assert.equal(response.status, 200);
+assert.equal(response.body.data, "User: 22");
 expect(response.status).toBe(200);
 expect(response.body.data).toBe("User: 23");
